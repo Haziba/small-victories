236f2d49389275b376e9defc256caae7aee7ac3a
Harry Boyes <harry.boyes@gmail.com>
2017-02-08 10:34:03 +0000

    Show days / answers on report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,10 @@
 class ReportController < ApplicationController
   def show
     @questionnaire = Questionnaire.find(params[:questionnaire_id])
+
+    answer_dates = @questionnaire.questions.flat_map { |q| q.answers.map(&:when) }
+
+    @start_date = answer_dates.min.to_date
+    @end_date = answer_dates.max.to_date
   end
 end
